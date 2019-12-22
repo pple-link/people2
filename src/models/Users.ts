46abe2b17dfa06b68315be5294d9b5914e006a41
Q00 <jqyu.lee@gmail.com>
2019-12-23 03:19:05 +0900

    feat: ⚡️Add User model

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,4 +1,48 @@
-import {
-  Column, Entity, JoinColumn, OneToMany, OneToOne,
-} from 'typeorm';
-import { BaseModel } from './BaseModel';
+import { Column, Entity, UpdateDateColumn, OneToMany } from "typeorm";
+import { BaseModel } from "./BaseModel";
+import { BaseBoard } from "./BaseBoard";
+import { BaseComment } from "./BaseComment";
+export enum IsAdmin {
+  Admin = "admin",
+  Normal = "normal"
+}
+
+export enum Blood {
+  RHPO = "o+",
+  RHPB = "b+",
+  RHPA = "a+",
+  RHPAB = "ab+",
+  RHMO = "o-",
+  RHMB = "b-",
+  RHMA = "a-",
+  RHMAB = "ab-"
+}
+
+@Entity()
+export abstract class User extends BaseModel {
+  @Column({ length: 45 })
+  public nickname!: string; // 닉네임
+  @Column({ length: 200 })
+  public profile!: string; // 프로필 이미지
+  @Column({ length: 25 })
+  public phone!: string;
+  @Column({ length: 35 })
+  public email!: string;
+  @Column({ type: "enum", enum: Blood })
+  public blood!: Blood;
+  @Column({ type: "enum", enum: IsAdmin })
+  public isAdmin!: IsAdmin;
+  @UpdateDateColumn()
+  public lastLoginDate!: Date;
+  @OneToMany(
+    _ => BaseBoard,
+    board => board.author
+  )
+  public boards?: BaseBoard[];
+
+  @OneToMany(
+    _ => BaseComment,
+    comment => comment.author
+  )
+  public comments?: BaseComment[];
+}
