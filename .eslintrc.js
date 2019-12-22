d0098c99987e30861f67a946db69bbd5d79a1652
Q00 <jqyu.lee@gmail.com>
2019-12-23 01:57:48 +0900

    refactor: 🛠lint eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
   extends: [
     // Airbnb style guide 적용
-    'airbnb-base',
+    "airbnb-base",
     // TypeScript ESLint recommanded style 적용
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "eslint-plugin-prettier"
   ],
   rules: {
-    'prettier:prettier': [
-      'error',
+    "prettier:prettier": [
+      "error",
       {
-        endOfLine: 'lf',
+        endOfLine: "lf",
         semi: true,
-        singleQuote: true,
         tabWidth: 2,
-        trailingComma: 'all',
+        trailingComma: "all",
         printWidth: 80,
-        arrowParens: 'always',
-        parser: 'babel',
+        arrowParens: "always",
+        parser: "babel",
         bracketSpacing: true,
         jsxBracketSameLine: true,
         jsxSingleQuote: true,
         useTabs: false,
-        htmlWhitespaceSensitivity: 'strict',
-      },
-    ],
-  },
+        htmlWhitespaceSensitivity: "strict"
+      }
+    ]
+  }
 };
