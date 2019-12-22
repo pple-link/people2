f5d663598ae54dc2e506183afb52d6a51826bade
Q00 <jqyu.lee@gmail.com>
2019-12-23 04:35:34 +0900

    feat: ⚡️add selectedBoard model

--- src/models/SelectedBoards.ts
+++ src/models/SelectedBoards.ts
@@ -1,4 +1,28 @@
-import {
-  Column, Entity, JoinColumn, OneToMany, OneToOne,
-} from 'typeorm';
-import { BaseModel } from './BaseModel';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from "typeorm";
+import { BaseBoard } from "./BaseBoard";
+import { Location, Blood, DonationKind } from "./Enum";
+import { CrawlBoard } from "./CrawlBoard";
+import { Participation } from "./Participations";
+@Entity()
+export abstract class SelectedBoard extends BaseBoard {
+  @Column({ type: "enum", enum: Location })
+  public location!: Location;
+  @Column({ type: "text" })
+  public hospital!: string;
+  @Column({ type: "enum", enum: Blood })
+  public blood!: Blood;
+  @Column({ type: "enum", enum: DonationKind })
+  public doationKind!: DonationKind;
+
+  @OneToMany(
+    _ => CrawlBoard,
+    crawlBoard => crawlBoard.board
+  )
+  public crwalBoard?: CrawlBoard[];
+
+  @OneToMany(
+    _ => Participation,
+    participation => participation.participateUserId
+  )
+  public participation?: Participation[];
+}
