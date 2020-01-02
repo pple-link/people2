05dca45abec80a18e077f41ad1f936d494d70e4e
Q00 <jqyu.lee@gmail.com>
2020-01-03 04:55:26 +0900

    feat: ⚡️ normal Board test

---
+++ src/tests/features/NormalBoardService.spec.ts
@@ -0,0 +1,52 @@
+import { connectDatabase } from "../../database";
+import { QueryRunner } from "typeorm";
+import { NormalBoardService, UserService } from "../../services";
+import { ShowFlag } from "../../models/Enum";
+let queryRunner: QueryRunner | null = null;
+
+beforeAll(async () => {
+  const conn = await connectDatabase();
+  queryRunner = conn.createQueryRunner();
+  await queryRunner.startTransaction();
+});
+
+describe("BoardService", () => {
+  it("new NormalBoard", async () => {
+    const boardService = new NormalBoardService();
+    const userService = new UserService();
+    const user = await userService.getByClientId("123");
+
+    const normalBoard = await boardService.save({
+      title: "테스트 게시글",
+      content: "안녕하세요 첫 글이에요~",
+      showFlag: ShowFlag["PENDING"],
+      user: user
+    });
+    delete normalBoard.id;
+    delete normalBoard.createdAt;
+    delete normalBoard.updatedAt;
+    delete normalBoard.deletedAt;
+    expect(normalBoard).toEqual({
+      title: "테스트 게시글",
+      content: "안녕하세요 첫 글이에요~",
+      showFlag: ShowFlag["PENDING"],
+      user: user,
+      comment: [],
+      reportCount: 0
+    });
+  });
+
+  it("get Board by user", async () => {
+    const boardService = new NormalBoardService();
+    const userService = new UserService();
+    const user = await userService.getByClientId("123");
+    const normalBoard = await boardService.getByUserId(user!.id);
+    console.log(normalBoard);
+  });
+});
+
+afterAll(async () => {
+  if (queryRunner) {
+    await queryRunner.release();
+  }
+});
