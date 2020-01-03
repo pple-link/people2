3d3da169ca34d8c1944c0d47426002049981ec71
Q00 <jqyu.lee@gmail.com>
2020-01-03 18:42:12 +0900

    refactor: 🛠 add path index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,6 @@
 export { UserService } from "./UserService";
-export { BoardService } from "./BoardService";
+export { BaseBoardService } from "./BaseBoardService";
+export { NormalBoardService } from "./NormalBoardService";
+export { DirectBoardService } from "./DirectBoardService";
+export { NoticeBoardService } from "./NoticeBoardService";
+export { FaqBoardService } from "./FaqBoardService";
