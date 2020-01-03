e4cae3fa30d7ba7d1c720eafb36439a1967bb78b
Q00 <jqyu.lee@gmail.com>
2020-01-03 18:39:18 +0900

    feat: ⚡️ DirectBoard service

---
+++ src/services/DirectBoardService.ts
@@ -0,0 +1,40 @@
+import { Service } from "typedi";
+import { IBoardDTO, BaseBoardService } from "./BaseBoardService";
+import { ShowFlag, Blood, Location } from "../models/Enum";
+import { DirectBoard, DirectBoardComment, User } from "../models";
+
+export interface IDirectBoardDTO extends IBoardDTO {
+  user: User;
+  comments: DirectBoardComment[];
+  location: Location;
+  hospital: string;
+  blood: Blood;
+  donationKinds: string;
+}
+
+@Service()
+export class DirectBoardService extends BaseBoardService<DirectBoard> {
+  constructor() {
+    super(DirectBoard);
+  }
+
+  public async save(board: Partial<IDirectBoardDTO>): Promise<DirectBoard> {
+    return this.genericRepository.save({
+      title: board.title,
+      content: board.content,
+      showFlag: ShowFlag["PENDING"],
+      user: board.user,
+      location: board.location,
+      hospital: board.hospital,
+      blood: board.blood,
+      donationKinds: board.donationKinds
+    });
+  }
+
+  public async getByUserId(userId: number): Promise<DirectBoard[]> {
+    return this.genericRepository.find({
+      where: { user: userId },
+      relations: [/*"directBoardComments"*/ "user"]
+    });
+  }
+}
