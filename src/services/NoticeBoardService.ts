49a44713d19a8a0b4dbdca77b44530d2e0d05faa
Q00 <jqyu.lee@gmail.com>
2020-01-06 21:43:48 +0900

    refactor: 🛠delete unread type

--- src/services/NoticeBoardService.ts
+++ src/services/NoticeBoardService.ts
@@ -4,8 +4,6 @@ import { IBoardDTO } from "./BaseBoardService";
 import { Notice } from "../models";
 import { ShowFlag } from "../models/Enum";
 
-export type ObjectType<T> = { new (): T } | Function;
-
 @Service()
 export class NoticeBoardService extends BaseBoardService<Notice> {
   constructor() {
