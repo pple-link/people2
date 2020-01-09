896ae8b31fa43111f2ca758fe622989e9aa712c2
Q00 <jqyu.lee@gmail.com>
2020-01-09 11:14:10 +0900

    refactor: 🛠 linting

--- src/models/BaseModel.ts
+++ src/models/BaseModel.ts
@@ -1,15 +1,19 @@
 import {
-  CreateDateColumn, Generated, PrimaryColumn, UpdateDateColumn, ValueTransformer,
-} from 'typeorm';
+  CreateDateColumn,
+  Generated,
+  PrimaryColumn,
+  UpdateDateColumn,
+  ValueTransformer
+} from "typeorm";
 
 const bigIntTransformer: ValueTransformer = {
   to: (entitiyValue: bigint) => entitiyValue,
-  from: (databaseValue: string) => parseInt(databaseValue, 10),
+  from: (databaseValue: string) => parseInt(databaseValue, 10)
 };
 
 export abstract class BaseModel {
-  @Generated('increment')
-  @PrimaryColumn({ type: 'bigint', transformer: [bigIntTransformer] })
+  @Generated("increment")
+  @PrimaryColumn({ type: "bigint", transformer: [bigIntTransformer] })
   public id!: number;
 
   @CreateDateColumn()
