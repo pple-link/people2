5db222a9ffabc6bdaf40c18a35263ace6c2bd4d7
Q00 <jqyu.lee@gmail.com>
2020-01-10 17:59:05 +0900

    feat: ⚡️ kakao test

---
+++ src/tests/provider/kakao.spec.ts
@@ -0,0 +1,34 @@
+import "../../utils/env";
+import { QueryRunner } from "typeorm";
+import { connectDatabase } from "../../database";
+import { KaKaoProvider } from "../../providers/KakaoProvider";
+import Container from "typedi";
+import { Authentication } from "../../utils/Authenticate";
+import { UserService } from "../../services";
+let queryRunner: QueryRunner | null = null;
+
+beforeAll(async () => {
+  const conn = await connectDatabase();
+  queryRunner = conn.createQueryRunner();
+  await queryRunner.startTransaction();
+});
+
+describe("Authenticate", () => {
+  it("generateToken", async () => {
+    const clientToken =
+      "PrHGn1KycTZAfWibiYK3PiWf2Py9DbLMB91F5QopdbMAAAFvjmvoQA";
+    const kakaoProvider = new KaKaoProvider();
+
+    const clientId = await kakaoProvider.getClient_id(clientToken);
+    const userService = Container.get(UserService);
+    const user = await userService.getByClientId(clientId);
+    const jwt = await kakaoProvider.generateToken(user.id);
+    expect(Authentication.verifyToekn(jwt)).toEqual(true);
+  });
+});
+
+afterAll(async () => {
+  if (queryRunner) {
+    await queryRunner.release();
+  }
+});
