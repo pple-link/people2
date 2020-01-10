ce4ec38016ba84dcdf2d72956c97e2833a4b3ccb
Q00 <jqyu.lee@gmail.com>
2020-01-10 19:47:48 +0900

    feat: ⚡️ user test (edit, delete )

--- src/tests/api/user.spec.ts
+++ src/tests/api/user.spec.ts
@@ -21,6 +21,27 @@ describe("user", () => {
     console.log(res.text);
     expect(res.status).toBeGreaterThanOrEqual(200);
   });
+  test("POST /edit", async () => {
+    const res = await supertest(app)
+      .post("/user/edit")
+      .send({
+        nickname: "안녕나는재규"
+      })
+      .set(
+        "authorization",
+        `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTU3ODY1MTUyNSwiZXhwIjoxNTc4NzM3OTI1fQ.KQzQcvK2h0Ggq-V8n9ZFIYtMpqBO3UqVkmmqWrB6gbnP71nLc0_WxI_lTUZyuAMC1e5lOTDNsXI67ozjo9W66A`
+      );
+    console.log(res.text);
+  });
+  test("POST /delete", async () => {
+    const res = await supertest(app)
+      .post("/user/delete")
+      .set(
+        "authorization",
+        `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTU3ODY1MTUyNSwiZXhwIjoxNTc4NzM3OTI1fQ.KQzQcvK2h0Ggq-V8n9ZFIYtMpqBO3UqVkmmqWrB6gbnP71nLc0_WxI_lTUZyuAMC1e5lOTDNsXI67ozjo9W66A`
+      );
+    console.log(res.text);
+  });
 });
 
 afterAll(async () => {
