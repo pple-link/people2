c35c153f70c1121c04d744dbd0f232877630f8cc
Q00 <jqyu.lee@gmail.com>
2020-01-11 17:44:00 +0900

    refactor: 🛠 set header add undefined

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-export const apiClient = (url: string, headers: Object) => {
+export const apiClient = (url: string, headers?: Object) => {
   return axios.create({
     baseURL: url,
-    headers: headers
+    headers: headers || {}
   });
 };
