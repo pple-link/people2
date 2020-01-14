00355267dbef380290c50eaefde30179e5be72b0
Q00 <jqyu.lee@gmail.com>
2020-01-14 11:38:28 +0900

    feat: ⚡️ add logging middleware

---
+++ src/middlewares/writeLog.ts
@@ -0,0 +1,37 @@
+import {
+  ExpressMiddlewareInterface,
+  Middleware,
+  Req,
+  Res
+} from "routing-controllers";
+
+import { NextFunction, Request, Response } from "express";
+import { ApiLogService } from "../services";
+import Container from "typedi";
+
+@Middleware({ type: "before" })
+export class StartTimerMiddleware implements ExpressMiddlewareInterface {
+  use(@Req() request: Request, @Res() _: Response, next: NextFunction): void {
+    console.log("timer is started.");
+    request.query.startTime = String(new Date().getTime());
+    next();
+  }
+}
+
+@Middleware({ type: "after" })
+export class endTimerMiddleware implements ExpressMiddlewareInterface {
+  async use(
+    request: Request,
+    @Res() _: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const time = new Date().getTime();
+    const delay = time - Number(request.query.startTime);
+    const apiLogService = Container.get(ApiLogService);
+    const user = request.query.user;
+    const url = request.url;
+
+    await apiLogService.save(user, url, delay);
+    next();
+  }
+}
