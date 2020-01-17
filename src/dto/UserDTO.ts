15c6f09505b823c6a6c1d34a45946f25e085b579
Q00 <jqyu.lee@gmail.com>
2020-01-17 13:42:04 +0900

    feat: user dto class

---
+++ src/dto/UserDTO.ts
@@ -0,0 +1,13 @@
+export class IUserDTOCLass {
+  public nickname: string;
+  public name: string;
+  public birthday: Date;
+  public profile: string;
+  public phone: string;
+  public email: string;
+  public sex: Sex;
+  public blood: Blood;
+  public job: Job;
+  public inflow: string;
+  public lastLoginDate: Date;
+}
