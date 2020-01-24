874b243690322a4200f0700de911926dfb45e1d6
Q00 <jqyu.lee@gmail.com>
2020-01-25 02:34:03 +0900

    refactor: 🛠 change usernum to user parameter

--- src/services/NormalBoardDepthCommentService.ts
+++ src/services/NormalBoardDepthCommentService.ts
@@ -1,6 +1,5 @@
 import { Service, Container } from "typedi";
 import { BaseCommentService, IDepthCommentDTO } from "./BaseCommentService";
-import { UserService } from "./UserService";
 import { NormalBoardDepthComment } from "../models";
 import { NormalBoardCommentService } from "./NormalBoardCommentService";
 
@@ -19,12 +18,10 @@ export class NormalBoardDepthCommentService extends BaseCommentService<
     const parentComment = await normalBoardCommentService.getById(
       depthComment.commentId
     );
-    const userService = Container.get(UserService);
-    const user = await userService.getById(depthComment.user.id);
     return this.genericRepository.save({
       comment: depthComment.comment,
       ref: parentComment,
-      user: user
+      user: depthComment.user
     }) as Promise<NormalBoardDepthComment>;
   }
 }
