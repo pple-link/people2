5ffab1fff39c75b2d55cb93c22c4d483c4af9e96
Q00 <jqyu.lee@gmail.com>
2020-01-30 05:20:57 +0900

    fix: 🔥 pagination login change and fix where

--- src/services/BaseBoardService.ts
+++ src/services/BaseBoardService.ts
@@ -2,8 +2,7 @@ import { Service } from "typedi";
 import { BaseService, ObjectType, listForm } from "./BaseService";
 import { ShowFlag } from "../models/Enum";
 import { BaseBoard } from "../models/BaseBoard";
-import { Like, Not, IsNull } from "typeorm";
-import _ from "lodash";
+import { Like, IsNull } from "typeorm";
 
 export interface IBoardDTO {
   title: string;
@@ -22,27 +21,31 @@ export abstract class BaseBoardService<T extends BaseBoard> extends BaseService<
     super(repo);
   }
 
-  public async getBoardList(
-    take: number,
-    skip: number,
-    query?: string
-  ): listForm<T> {
+  public async getBoardList(page: number, query?: string): listForm<T> {
+    const size = 10;
+    const begin = (page - 1) * size;
+
     let board_list;
     if (query) {
       board_list = await this.getByWhere(
-        {
-          where: [
-            { title: Like(`%${query}%`) },
-            { content: Like(`%${query}%`) },
-            { deletedAt: Not(IsNull()) }
-          ]
-        },
+        [
+          {
+            title: Like(`%${query}%`),
+            deletedAt: IsNull(),
+            showFlag: ShowFlag["SHOW"]
+          },
+          {
+            content: Like(`%${query}%`),
+            deletedAt: IsNull(),
+            showFlag: ShowFlag["SHOW"]
+          }
+        ],
         ["user"],
-        take,
-        skip
+        begin,
+        size
       );
     } else {
-      board_list = await this.list(["user"], take, skip);
+      board_list = await this.list(["user"], begin, size);
     }
 
     return { array: board_list[0], total: board_list[1] };
